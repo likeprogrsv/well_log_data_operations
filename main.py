a06710905246aab09bceeff9ae1f32c5fc5fe93f
progrsv <likeprogrsv@gmail.com>
2022-12-29 18:40:16 +0600

    add coordinates columns to result file

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ result_sheet1_name = 'sheet1'
 result_sheet2_name = 'sheet2'
 errors_sheet3_name = 'errors'
 sheet1_columns = ['oil field', 'well', 'UWI', 'depth', 'T', 'AO', 'GK', 'NKT', 'IK', 'S1+S2', 'TOC']
-sheet2_columns = ['oil field', 'well', 'UWI', 'depth top', 'depth bot', 'T', 'AO', 'GK mean', 'NKT mean', 'IK mean', \
+sheet2_columns = ['oil field', 'well', 'UWI', 'X', 'Y', 'depth top', 'depth bot', 'T', 'AO', 'GK mean', 'NKT mean', 'IK mean', \
     'S1+S2 mean', 'TOC mean']
 
 # Переменная для операции по вычисению натурального логарифма
@@ -54,6 +54,8 @@ curves_errors = {}
 param = {
             'oil_field': 'Месторождение',
             'well_name': 'Скважина',
+            'x_coord': 'X на подошву',
+            'y_coord': 'Y на подошву',
             'T': 'Т',
             'AO': 'АО',
             'depth_top': 'Кровля репера',
@@ -69,8 +71,8 @@ project_path = os.path.abspath('.')
 
 def curr_well_excel_data(las_uwi):
     '''Извлекаем из экселя необходимые данные по текущей скважине'''
-    data = excel_df.loc[excel_df['UWI'] == las_uwi, (param['oil_field'], param['well_name'], param['T'], \
-        param['depth_top'], param['depth_bot'], param['AO'], param['abs_top'] , param['abs_bot'])]
+    data = excel_df.loc[excel_df['UWI'] == las_uwi, (param['oil_field'], param['well_name'], param['T'], param['x_coord'], \
+       param['y_coord'], param['depth_top'], param['depth_bot'], param['AO'], param['abs_top'] , param['abs_bot'])]
     if data.empty:
         return print(bcolors.bcolors.FAIL + f'-----------\nLAS WELL UWI:{las_uwi} NOT FOUND IN EXCEL!!! \
         \n-----------\n' + bcolors.bcolors.ENDC)
@@ -163,6 +165,10 @@ if __name__ == "__main__":
                 try:
                     las = lasio.read(las_files_path + '/' + filename.name, encoding='cp866')    
                 except ValueError:
+                    # Записываем ошибки
+                    errors_df = pd.DataFrame({'Скв отсутст в экселе': '-', 'Скв с отриц знач в кривых': '-', \
+                        'Не правильная форма данных кривых': filename.name}, index=[writer.sheets[errors_sheet3_name].max_row])
+                    write_results(writer, errors_sheet3_name, errors_df)
                     print(bcolors.bcolors.FAIL + f'-----------\nЧто-то не так с данными кривых в ласе скважины: {filename.name}!!! \
                         \n-----------\n' + bcolors.bcolors.ENDC)
                     continue            
@@ -173,12 +179,12 @@ if __name__ == "__main__":
 
                 # Для удобства присвоим переменным значения нужных параметров из экселя со скважинами
                 try:
-                    oil_field, well_name, t, depth_t, depth_b, ao, abs_t, abs_b = (float(well_excel[i].to_numpy()) if well_excel[i].dtype != \
-                        'object' else well_excel[i].values.astype(str)[0] for i in well_excel)
+                    oil_field, well_name, t, x_coord, y_coord, depth_t, depth_b, ao, abs_t, abs_b = (float(well_excel[i].to_numpy()) \
+                        if well_excel[i].dtype != 'object' else well_excel[i].values.astype(str)[0] for i in well_excel)
                 except TypeError:                            
                     # Записываем ошибки
-                    errors_df = pd.DataFrame({'Скв отсутст в экселе': filename.name, 'Скв с отриц знач в кривых': '-'}, \
-                        index=[writer.sheets[errors_sheet3_name].max_row])
+                    errors_df = pd.DataFrame({'Скв отсутст в экселе': filename.name, 'Скв с отриц знач в кривых': '-', \
+                        'Не правильная форма данных кривых': '-'}, index=[writer.sheets[errors_sheet3_name].max_row])
                     write_results(writer, errors_sheet3_name, errors_df)
                     continue
 
@@ -203,7 +209,7 @@ if __name__ == "__main__":
                         ik[row], s1_s2_result, toc_result], index=sheet1_columns).T                
                     sheet1_df = pd.concat([sheet1_df, new_row], ignore_index=True)
 
-                row_for_sheet2 = pd.DataFrame([oil_field, well_name, las_uwi, depth_t, depth_b, t, ao, sheet1_df['GK'].mean(), \
+                row_for_sheet2 = pd.DataFrame([oil_field, well_name, las_uwi, x_coord, y_coord, depth_t, depth_b, t, ao, sheet1_df['GK'].mean(), \
                     sheet1_df['NKT'].mean(), sheet1_df['IK'].mean(), sheet1_df['S1+S2'].mean(), sheet1_df['TOC'].mean()], index=sheet2_columns).T
                 sheet2_df = pd.concat([sheet2_df, row_for_sheet2], ignore_index=True)
                 
@@ -215,61 +221,9 @@ if __name__ == "__main__":
 
                 # Записываем ошибки
                 if filename.name in curves_errors:   
-                    errors_df = pd.DataFrame({'Скв отсутст в экселе': '-', 'Скв с отриц знач в кривых': well_name}, \
-                        index=[writer.sheets[errors_sheet3_name].max_row])
+                    errors_df = pd.DataFrame({'Скв отсутст в экселе': '-', 'Скв с отриц знач в кривых': well_name, \
+                        'Не правильная форма данных кривых': '-'}, index=[writer.sheets[errors_sheet3_name].max_row])
                     write_results(writer, errors_sheet3_name, errors_df)    
         
     total_time = datetime.now() - start
     print(bcolors.bcolors.OKBLUE + f'Время работы программы: {total_time}' + bcolors.bcolors.ENDC)
-
-
-'''
-            with pd.ExcelWriter(resulting_file_name, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-                # Записываем первую книгу отчета со всеми строками данных
-                if writer.sheets[result_sheet1_name].max_row > 1:
-                    sheet1_df.to_excel(writer, sheet_name=result_sheet1_name, startrow=writer.sheets[result_sheet1_name].max_row, \
-                        header=False, index=False)           
-                else:            
-                    sheet1_df.to_excel(writer, sheet_name=result_sheet1_name, index=False)
-                
-                # Записываем вторую сводную книгу отчета со средними рассчитанными значениями по каждой скважине
-                if writer.sheets[result_sheet2_name].max_row > 1:  
-                    sheet2_df.to_excel(writer, sheet_name=result_sheet2_name, startrow=writer.sheets[result_sheet2_name].max_row, \
-                        header=False, index=False)                          
-                else:
-                    sheet2_df.to_excel(writer, sheet_name=result_sheet2_name, index=False)
-'''
-    # errors_df = pd.DataFrame({'Скв отсутст в экселе': well_not_found, 'Скв с отриц знач в кривых': curves_errors})
-    # with pd.ExcelWriter(resulting_file_name, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-    #     errors_df.to_excel(writer, sheet_name=errors_sheet3_name, index=False)
-
-
-
-"""
-CURVES_MNEMONIC = {             
-    'GK': {'GK'},
-    'IK': {'IK'},
-    'NKT': {'NKT', 'NKTB'},
-    }
-
-
-
-# print(curr_well['Т'].iloc[0])         # доступ к числовому значению нужного параметра из экселя
-
-
-
-
-wb = load_workbook(filename='wells_data.xlsx', read_only=True)
-ws = wb['Лист 1']
-
-
-def get_headers():
-    ''' Load headers from excel file'''
-    headers = {}
-    for row in ws.iter_rows(max_col=ws.max_column, max_row=1):
-        for cell in row:       
-            headers[cell.value] = cell.column
-    return headers
-    
-       
-"""
